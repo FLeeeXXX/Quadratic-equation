7f5c370de8449d53f098c54f8637ca50da9be377
FLeeeXXX <43463155+FLeeeXXX@users.noreply.github.com>
2023-02-22 12:40:12 +0500

    Initial Commit

---
+++ Quadratic equation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quadratic equation
+//
+//  Created by Алексей Шульгин on 22.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
